b725cc9e8131e054bdedc6587f97207029d87189
Chenkehan21 <751722804@qq.com>
2024-05-22 12:10:10 +0000

    sota 0.243

--- run_r2r/main2.sh
+++ run_r2r/main2.sh
@@ -1,16 +1,16 @@
 export GLOG_minloglevel=2
 export MAGNUM_LOG=quiet
 
-flag=" --exp_name exp_mp_1839_0.5value_map
+flag=" --exp_name exp_mp_1839_history_map
       --run-type eval
       --exp-config vlnce_baselines/config/exp1.yaml
-      --nprocesses 12
+      --nprocesses 16
       NUM_ENVIRONMENTS 1
       TRAINER_NAME ZS-Evaluator-mp
-      TORCH_GPU_IDS [0,1,2,3,4,5]
-      SIMULATOR_GPU_IDS [0,1,2,3,4,5]
+      TORCH_GPU_IDS [0,1,2,3,4,5,6,7]
+      SIMULATOR_GPU_IDS [0,1,2,3,4,5,6,7]
       "
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6 python run_mp.py $flag
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run_mp.py $flag
 
 # flag=" --exp_name exp_1_choice_check
 #       --run-type eval
